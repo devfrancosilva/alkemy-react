188e3d0046c5d107f3622d95e995952c1243eabf
Franco Silva <dev.francosilva@gmail.com>
2021-10-01 12:04:36 -0300

    headers API

--- src/API.js
+++ src/API.js
@@ -2,8 +2,16 @@ import axios from "axios";
 
 export const API_AUTH = axios.create({
   baseURL: "http://challenge-react.alkemy.org",
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
 });
 
 export const API = axios.create({
   baseURL: "https://superheroapi.com/api/10215933128600480",
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
 });
